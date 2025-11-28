0fae7e03d59e722ebc527fc2e68c9e156950ed59
Prashant Gadhesariya <48445326+PMGadhesariy96@users.noreply.github.com>
2025-11-28 23:02:30 +0530

    Removed add lead option in sidebar

--- resources/views/sections/menu.blade.php
+++ resources/views/sections/menu.blade.php
@@ -45,9 +45,6 @@
                 <x-sub-menu-item :link="route('lead-dashboard.index')" :text="__('app.leadDashboard')" />
                 <x-sub-menu-item :link="route('lead-contact.index')" :text="__('app.leadContact')" />
                 <x-sub-menu-item :link="route('lead-list.index')" :text="__('app.leadList')" />
-                @if (in_array(user()->permission('add_lead'), ['all', 'added']))
-                <x-sub-menu-item :link="route('add-lead.index')" :text="__('app.addLead')" />
-                @endif
 
             </div>
             @endif
